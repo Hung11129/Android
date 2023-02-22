bcb4a9fdcf6d97f4b06c46d189827db88320ec58
Hung11129 <vanhung160306@gmail.com>
2023-02-22 23:27:05 +0700

    Hello 2

--- music4b-main/music4b-main/app/src/main/java/com/example/btl_music4b/Service_API/APIService.java
+++
@@ -1,8 +0,0 @@
-package com.example.btl_music4b.Service_API;
-
-public class APIService {
-    private static String base_url = "https://music4b.000webhostapp.com/Server/";
-    public static Dataservice getService(){
-        return APIRetrofitClient.getClient(base_url).create(Dataservice.class);
-    }
-}
